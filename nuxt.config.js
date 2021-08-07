38cc08b43ddceb9a9128596731ab0248b167d758
carol liao <desire11012@gmail.com>
2021-08-07 14:03:15 +0800

    新增 scss helpers

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/assets/scss/all.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -38,4 +39,5 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
+  
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,117 @@
 <template>
-  <Tutorial/>
+  <div>
+    <!-- <Header /> -->
+    <main>
+      <section class="video-area">
+        <div class="container">
+          <ul class="wrap">
+            <li>
+              <a href="#">
+                <img src="https://i.ytimg.com/vi/d_DL7v4veE4/hq720_live.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAq0HSqnktqdRWEeIVuhK81S1edXw" alt="billboard 2021">
+                <p class="time-tag">2:00:14</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="https://i.ytimg.com/vi/-MwBCZKpGoU/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDTOpMDOrM7f5O5TXsfxD_v61Utow" alt="songs 2021">
+                <p class="time-tag">1:30</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="https://i.ytimg.com/vi/tRF2Xbx9ccM/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAkRPwf-QBRasWutdNoSmwAYn2x9w" alt="new songs 2021">
+                <p class="time-tag">2:00:14</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="https://i.ytimg.com/vi/dRZ_QRSrb5s/hq720_live.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBtriVq3QxCxb_ONHTwbJMiTYAIqg" alt="worship songs">
+                <p class="time-tag">1:30:05</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="https://i.ytimg.com/vi/-jVSv77XkXA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDTji_uE21UMM9PnBGwffBtmh9unw" alt="the fat rat">
+                <p class="time-tag">15:16</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="https://i.ytimg.com/vi/xIEhVH7wQw4/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCjJA-0RjsVWz42vW_HOVw8I58hhA" alt="popular songs">
+                <p class="time-tag">2:00:14</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="https://i.ytimg.com/vi/Ny81HYQ1i8w/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD8Z91AfmhTKYZpgQJv-BgG_AeYVA" alt="chillvibes">
+                <p class="time-tag">20:33</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="https://i.ytimg.com/vi/6GLaO8V6gfA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCAC-a-dFjju9ZAoaZso9SxF74S-w" alt="englisg songs">
+                <p class="time-tag">3:00:00</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="https://i.ytimg.com/vi/d_DL7v4veE4/hq720_live.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAq0HSqnktqdRWEeIVuhK81S1edXw" alt="billboard 2021">
+                <p class="time-tag">2:00:14</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="https://i.ytimg.com/vi/-MwBCZKpGoU/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDTOpMDOrM7f5O5TXsfxD_v61Utow" alt="songs 2021">
+                <p class="time-tag">1:30</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="https://i.ytimg.com/vi/tRF2Xbx9ccM/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAkRPwf-QBRasWutdNoSmwAYn2x9w" alt="new songs 2021">
+                <p class="time-tag">2:00:14</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="https://i.ytimg.com/vi/dRZ_QRSrb5s/hq720_live.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBtriVq3QxCxb_ONHTwbJMiTYAIqg" alt="worship songs">
+                <p class="time-tag">1:30:05</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="https://i.ytimg.com/vi/-jVSv77XkXA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDTji_uE21UMM9PnBGwffBtmh9unw" alt="the fat rat">
+                <p class="time-tag">15:16</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="https://i.ytimg.com/vi/xIEhVH7wQw4/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCjJA-0RjsVWz42vW_HOVw8I58hhA" alt="popular songs">
+                <p class="time-tag">2:00:14</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="https://i.ytimg.com/vi/Ny81HYQ1i8w/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD8Z91AfmhTKYZpgQJv-BgG_AeYVA" alt="chillvibes">
+                <p class="time-tag">20:33</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="https://i.ytimg.com/vi/6GLaO8V6gfA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCAC-a-dFjju9ZAoaZso9SxF74S-w" alt="englisg songs">
+                <p class="time-tag">3:00:00</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </main>
+    <!-- <Footer /> -->
+  </div>
 </template>
 
 <script>
+// import Header from '@/components/Header.vue';
+// import Footer from '@/components/Footer.vue';
+
 export default {}
 </script>
